de94638fc8cd7fe049ee778bc67b5221983e2b6a
taimoor948 <taimoorhabib948@gmail.com>
2025-04-14 19:27:04 +0500

    updatelandingpage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from "../components/Navbar";
+
 const AboutPage = () => {
     return (
       <div className="p-8 text-black">
+        <Navbar/>
         <h1 className="text-3xl font-bold">About Us</h1>
         <p>This is the about page of Universities & Boards Department.</p>
       </div>
